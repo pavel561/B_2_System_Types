4e46179b9613858d79a801bc99d4ffea3cf02ece
PavelGornostaev <pavel561@gmail.com>
2018-12-12 16:53:36 +0300

    add 2.4 STRING to INT add 3.1 BOOL to CHAR

--- Practice.cs
+++ Practice.cs
@@ -52,11 +52,9 @@ namespace ConsoleApp7
 
 			//1. CHAR CONVERSION 
 			//1.1 CHAR to STRING
-
 			//string charToStringImplicit = sex;                        //IMPLICIT: NOT COMPILING
 			string charToStringImplicit = sex.ToString();               //Преобразование с использованием встроенного метода
 			string charToStringImplicit2 = Convert.ToString(sex);		//Преобразование с использованием метода класса System.Convert
-
 			//string chartToStringExplicit = (string)sex;               //EXPLICIT: NOT COMPILING
 			string charToStringUsingConverter = Convert.ToString(sex);  //CONVERT: "M"
 
@@ -80,26 +78,47 @@ namespace ConsoleApp7
 			int char_to_int4 = place;
 			int char_to_int5 = Convert.ToInt32(place);
 			//int char_to_int6 = Convert.ToInt32(sex);
+			//int char_to_int7 = Int32.Parse(sex);			//Отсутствует необходимый перегруженный метод  =(
 
 			//2. STRING CONVERSION
-
 			//2.1 STRING to CHAR
-			//char string_to_char = Convert.ToChar(hasPhoto);		//Исключение.Длина строки должна составлять один символ.
+			//char string_to_char = Convert.ToChar(hasPhoto);					//Исключение.Длина строки должна составлять один символ.
 			string string_one_sym = "M";
-			char string_to_char = Convert.ToChar(string_one_sym);       //результат 'T'
-			char string_to_char2 = hasPhoto.ToCharArray()[0];		//результат 'T'
+			char string_to_char = Convert.ToChar(string_one_sym);				//результат 'T'
+			char string_to_char2 = hasPhoto.ToCharArray()[0];					//результат 'T'
 			char string_to_char3 = Convert.ToChar(hasPhoto.Substring(0, 1));    //результат 'T'
-
+			char string_to_char4 = Char.Parse(hasPhoto);
+			char string_to_char5 = Char.Parse(string_one_sym);
 
 			//2.2 STRING to BOOL
+			bool str_to_bool = Convert.ToBoolean(hasPhoto);
+			bool str_to_bool2 = ((hasPhoto == "True") || (hasPhoto == "true")) ? true : false;
+			bool str_to_bool3 = Boolean.Parse(hasPhoto);
 
 			//2.3 STRING to DECIMAL
+			decimal str_to_decimal = Decimal.Parse(hasPhoto);
+			decimal str_to_decimal2 = Convert.ToDecimal(hasPhoto);
+			decimal str_to_decimal3 = Decimal.Parse(flatNumber);
+			decimal str_to_decimal4 = Convert.ToDecimal(flatNumber);
+			//decimal str_to_decimal5 = (decimal)flatNumber;	
 
 			//2.4 STRING to INT
+			int str_to_int = Int32.Parse(flatNumber);
+			int str_to_int2 = Convert.ToInt32(flatNumber);
+			// Можно  в цикле через Substring или ToCharArray вытягивать каждый символ из строки и умножая каждый на нужную степень, прибавлять к decimal переменной
+			decimal str_to_decimal6 = 0;
+			//for (int i = 0; i < decimal_str.Length; i++)
+			//{
+			//str_to_d
+			//}
+
 
 			//3. BOOL CONVERSION
 
 			//3.1 BOOL to CHAR
+			char bool_to_char = Convert.ToChar(hasFree2Pages);          //Исключение
+			char bool_to_char2 = hasFree2Pages?  'T': 'F';              //
+			char bool_to_char2 = hasFree2Pages ? '1' : '0';             //
 
 			//3.2 BOOL to STRING
 
